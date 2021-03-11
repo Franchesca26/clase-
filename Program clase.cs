7d65f99019e9425bbe18333f9da5c49affeda893
Franchesca26 <78986079+Franchesca26@users.noreply.github.com>
2021-03-10 23:56:50 -0400

    Clase
    Luisa Franchesca Rosario 2019-9012 (Miguel Moreta)

---
+++ Program clase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Clase
+{
+    class cliente
+    {
+        private string nombre;
+        private int monto;
+
+        public cliente(string nom)
+        {
+            nombre = nom;
+            monto = 0;
+        }
+
+        public void Depositar(int m)
+        {
+            monto = monto + m;
+        }
+
+        public void Extraer(int m)
+        {
+            monto = monto - m;
+        }
+
+        public int RetornarMonto()
+        {
+            return monto;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine(nombre + " tiene depositado la suma de " + monto);
+        }
+    }
+
+    class Banco
+    {
+        private cliente cliente1, cliente2, cliente3;
+
+        public Banco()
+        {
+            cliente1 = new cliente("Juan");
+            cliente2 = new cliente("Ana");
+            cliente3 = new cliente("Pedro");
+        }
+
+        public void Operar()
+        {
+            cliente1.Depositar(100);
+            cliente2.Depositar(150);
+            cliente3.Depositar(200);
+            cliente3.Extraer(150);
+        }
+
+        public void DepositosTotales()
+        {
+            int t = cliente1.RetornarMonto() +
+                    cliente2.RetornarMonto() +
+                    cliente3.RetornarMonto();
+            Console.WriteLine("El total de dinero en el banco es:" + t);
+            cliente1.Imprimir();
+            cliente2.Imprimir();
+            cliente3.Imprimir();
+        }
+
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+
+    }
+}
